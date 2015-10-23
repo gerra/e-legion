c5e63ed4201b5bfa6c4a17349b2f3badc6a8825c
gerra <gerralizza@gmail.com>
2015-10-23 13:28:26 +0300

    some design refactoring

--- VKPlaylister/app/src/main/java/ru/projects/german/vkplaylister/adapter/LocalAlbumListAdapter.java
+++ VKPlaylister/app/src/main/java/ru/projects/german/vkplaylister/adapter/LocalAlbumListAdapter.java
@@ -22,7 +22,7 @@ public class LocalAlbumListAdapter extends RecyclerView.Adapter<AlbumViewHolder>
 
     @Override
     public AlbumViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_playlist, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_album, parent, false);
         return new AlbumViewHolder(view);
     }
 
